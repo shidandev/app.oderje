0b4c837d1eff4446c405fbb8910e29a50f3c6686
shidandev <shidan95.dev@gmail.com>
2020-01-16 13:01:42 +0800

    folder css and js created

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,51 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+Route::get('/img/basic/{filename}', function($filename){
+    $path = resource_path() . '/image/basic/' . $filename;
+
+    if(!File::exists($path)) {
+        return response()->json(['message' => 'Image not found.'], 404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
+
+Route::get('/css/{filename}', function($filename){
+    $path = resource_path() . '/css/' . $filename;
+
+    if(!File::exists($path)) {
+        return response()->json(['message' => 'css file not found.'], 404);
+    }
+
+    $file = File::get($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", 'text/css');
+
+    return $response;
+});
+
+Route::get('/js/{filename}', function($filename){
+    $path = resource_path() . '/js/' . $filename;
+
+    if(!File::exists($path)) {
+        return response()->json(['message' => 'javascript file not found.'], 404);
+    }
+
+    $file = File::get($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", 'text/js');
+
+    return $response;
 });
